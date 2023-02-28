e1bf6d0edcc925473a5b8a02e0e8e37b906e21d3
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-02-28 13:44:20 +0200

    Fix

--- src/services/fetchMovies.js
+++ src/services/fetchMovies.js
@@ -1,34 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
 
-const searchParams = new URLSearchParams({
-    api_key: '151d7d67eff269279b50cee772990c55',
-    language: 'en-US',
-    include_adult: false,
-});
+const defaultParams = {
+  api_key: '151d7d67eff269279b50cee772990c55',
+  language: 'en-US',
+  include_adult: false,
+};
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = defaultParams;
 
 export const getTrendingMovies = async () => {
-  const { data } = await axios.get(`/trending/all/week?${searchParams}`);
+  const { data } = await axios.get('/trending/all/week');
   return data.results;
 };
 
 export const getMovieDetails = async id => {
-  const { data } = await axios.get(`/movie/${id}?${searchParams}`);
+  const { data } = await axios.get(`/movie/${id}`);
   return data;
 };
 
 export const getMovieByName = async name => {
-  const { data } = await axios.get(`/search/movie?${searchParams}&query=${name}`);
+  const { data } = await axios.get('/search/movie', {
+    params: { ...defaultParams, query: name },
+  });
   return data.results;
 };
 
 export const getMovieCast = async id => {
-  const { data } = await axios.get(`/movie/${id}/credits?${searchParams}`);
+  const { data } = await axios.get(`/movie/${id}/credits`);
   return data.cast;
 };
 
 export const getMovieReviews = async id => {
-  const { data } = await axios.get(`/movie/${id}/reviews?${searchParams}`);
+  const { data } = await axios.get(`/movie/${id}/reviews`);
   return data.results;
 };
